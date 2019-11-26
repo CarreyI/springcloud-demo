019b575db2f65ca5d571baced9906731c6dbcb62
carrey <carreyi@163.com>
2019-11-26 16:29:40 +0800

    1

--- springcloud-api-gateway/src/main/java/com/springcloud/gateway/Application.java
+++ springcloud-api-gateway/src/main/java/com/springcloud/gateway/Application.java
@@ -1,13 +1,11 @@
 package com.springcloud.gateway;
 
-import com.springcloud.gateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
-import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
@@ -20,13 +18,13 @@ public class Application {
 //        return new AccessFilter();
 //    }
 
-//    @Bean
-//    @RefreshScope
-//    @ConfigurationProperties("zuul")
-//    @Primary
-//    public ZuulProperties zuulProperties(){
-//        return new ZuulProperties();
-//    }
+    @Bean
+    @RefreshScope
+    @ConfigurationProperties("zuul")
+    @Primary
+    public ZuulProperties zuulProperties(){
+        return new ZuulProperties();
+    }
 
 //    @Bean
 //    public PatternServiceRouteMapper serviceRouteMapper(){
